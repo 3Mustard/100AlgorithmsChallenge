65cd5b908f7932a32c22df64183a8de5f8b4fecb
3Mustard <m23.cullen@gmail.com>
2020-04-09 13:32:23 -0400

    Solved both problems in add

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,14 @@
 function add(param1: number, param2: number): number {
-
+    return param1 + param2;
 }
 
-// function add2(param1: number[]): number {
-
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
+function add2(...param1: number[]): number {
+    return param1.reduce((sum: number, currentElement: number) => {
+        return sum + currentElement;
+    });
+}
+//  console.log(add(1, 2));
+//  console.log(add(3, 2));
 
 // console.log(add2(1,2,3,4,5));
 // console.log(add2(2,3));
-
