04b04510e4bc1d14b1a6634f56f86d44ca35a38a
3Mustard <m23.cullen@gmail.com>
2020-04-09 18:17:15 -0400

    solved addTwoDigits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,5 @@
 function addTwoDigits(n: any): number {
-
+    return n.toString().split("").reduce((x: string, y: string): number => {return parseInt(x, 10) + parseInt(y, 10);})
 }
 
 // console.log(addTwoDigits(29));
