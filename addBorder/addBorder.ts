2a740cbe55ae623cd6b970be7f50947e77f06354
3Mustard <m23.cullen@gmail.com>
2020-04-09 13:49:17 -0400

    solved addBorder

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,7 @@
 function addBorder(picture: string[]): string[] {
+    const topBotLength: number = picture[0].length + 2;
 
+    return ["*".repeat(topBotLength), "*" + picture[0] + "*", "*" + picture[1] + "*", "*".repeat(topBotLength)];
 }
 
 // console.log(addBorder(["abc", "ded"]));
