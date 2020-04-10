d2d5274f8a83f18447a123bde64ff25d0e1c68ec
3Mustard <m23.cullen@gmail.com>
2020-04-10 00:03:59 -0400

    solved adjacentElementsProduct

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,11 @@
 function adjacentElementsProduct(inputArray: number[]): number {
+    let largestProduct: number = inputArray[0] * inputArray[1];
 
+    for (let i = 1; i < inputArray.length - 1; i++) {
+        const testProduct: number = inputArray[i] * inputArray[i + 1];
+        largestProduct = testProduct > largestProduct ? testProduct : largestProduct;
+    }
+    return largestProduct;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+ console.log(adjacentElementsProduct([1, 6, 1, 1, 7, 3]));
