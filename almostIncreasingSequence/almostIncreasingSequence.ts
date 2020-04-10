8118ba6bc2f603c32d78920c4b407f77615b88f4
3Mustard <m23.cullen@gmail.com>
2020-04-10 18:48:46 -0400

    solved almostIncreaseSequence

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,18 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
+    let count: number = 0;
 
+    for(let i = 0; i < sequence.length; i++) {
+        if(sequence[i] <= sequence[i - 1]) {
+            count++;
+            if(sequence[i] <= sequence[i - 2] && sequence[i + 1] <= sequence[i - 1]) {
+                return false;
+            }
+        }
+    }
+
+    return count <= 1;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+
+//  console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+//  console.log(almostIncreasingSequence([1, 3, 5,6,7,4,8,9,55,66])) 
