2471bed473da6c44f33ed7068c1df06fb2f704e7
3Mustard <m23.cullen@gmail.com>
2020-04-16 00:53:47 -0400

    solved alphabetSubSequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,14 @@
 function alphabetSubsequence(s: string): boolean {
-
+    for (let i = 1; i < s.length - 1; i++) {
+        if (s.charCodeAt(i) < s.charCodeAt(i - 1)) {
+            return false;
+        }
+    }
+    return true;
 }
 
 console.log(alphabetSubsequence('zab'))
 console.log(alphabetSubsequence('effg'))
 console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
+console.log(alphabetSubsequence('aacccee'))
 console.log(alphabetSubsequence('bxz'))
