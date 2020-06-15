fd44b261b115fd0218820b006131c999d33ba1bb
3Mustard <m23.cullen@gmail.com>
2020-06-15 19:13:24 -0400

    comment out console logs

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -22,6 +22,6 @@ function areSimilar(a: number[], b: number[]): boolean {
    return false;
 }
 
-console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+// console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+// console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+// console.log(areSimilar([1, 2, 2], [2, 1, 1]));
