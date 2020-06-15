1e66c34a8e3ada3748913fb763cf96385969cff8
3Mustard <m23.cullen@gmail.com>
2020-06-15 17:25:32 -0400

    completed areEquallyStrong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,5 +1,10 @@
 function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+    const yourStrongest = yourLeft >= yourRight ? yourLeft : yourRight;
+    const yourWeakest = yourLeft <= yourRight ?  yourLeft : yourRight;
+    const friendStrongest = friendsLeft >= friendsRight ? friendsLeft : friendsRight;
+    const friendWeakest = friendsLeft <= friendsRight ? friendsLeft : friendsRight;
 
+    return ( yourStrongest === friendStrongest && yourWeakest === friendWeakest );
 }
 
 // console.log(areEquallyStrong(10, 15, 15, 10))
