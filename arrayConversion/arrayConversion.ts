5ec024e86eab25e14e5e1d269389ad2102383a94
3Mustard <m23.cullen@gmail.com>
2020-06-16 13:59:02 -0400

    solved iterationConversion

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -1,9 +1,17 @@
 function arrayConversion(inputArray: number[]): number {
-    
+    let iterationCount: number = 1;
+
+    while ( inputArray.length > 1 ) {
+        const action: string = iterationCount % 2 === 0 ? 'EVEN_ITERATION' : 'ODD_ITERATION';
+        inputArray = iterationAction(inputArray, action);
+        iterationCount++;
+    }
+    return inputArray[0];
 }
 
 function iterationAction(inputArray: number[], action: string): number[] {
     const result: number[] = [];
+
     for ( let i = 0; i < inputArray.length && inputArray.length >= 2; i += 2 ) {
         switch(action) {
             case 'ODD_ITERATION':
@@ -17,5 +25,6 @@ function iterationAction(inputArray: number[], action: string): number[] {
     return result;
 }
 
-console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
-console.log(iterationAction([1, 2, 3, 4, 5, 6, 7, 8], 'EVEN_ITERATION'));
+// console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+// console.log(iterationAction([1, 2, 3, 4, 5, 6, 7, 8], 'EVEN_ITERATION'));
+// console.log(iterationAction([1, 2, 3, 4, 5, 6, 7, 8], 'ODD_ITERATION'));
