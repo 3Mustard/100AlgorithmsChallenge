a9fcd80a023cf07c9b04f6f05deed18bca1462c2
3Mustard <m23.cullen@gmail.com>
2020-06-16 13:50:41 -0400

    removed odditeration function and replaced with iterationAction function

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -2,13 +2,20 @@ function arrayConversion(inputArray: number[]): number {
     
 }
 
-function oddIteration(inputArray: number[]): number[] {
+function iterationAction(inputArray: number[], action: string): number[] {
     const result: number[] = [];
     for ( let i = 0; i < inputArray.length && inputArray.length >= 2; i += 2 ) {
-        result.push(inputArray[i] + inputArray[i + 1]);
+        switch(action) {
+            case 'ODD_ITERATION':
+                result.push(inputArray[i] + inputArray[i + 1]);
+              break;
+            case 'EVEN_ITERATION':
+                result.push(inputArray[i] * inputArray[i + 1]);
+              break;
+          }
     }
     return result;
 }
 
 console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
-console.log(oddIteration([1, 2, 3, 4, 5, 6, 7, 8]));
+console.log(iterationAction([1, 2, 3, 4, 5, 6, 7, 8], 'EVEN_ITERATION'));
