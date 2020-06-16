6b20048aaa753ce71d66e67bc59a789e00dd4823
3Mustard <m23.cullen@gmail.com>
2020-06-15 22:43:37 -0400

    completed arrayChanges

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,15 @@
 function arrayChange(inputArray: number[]): number {
- 
+    let moves: number = 0;
+
+    for ( let i = 0; i < inputArray.length; i++ ) {
+        const current: number = inputArray[i];
+        let next: number = inputArray[i + 1];
+        if ( next !== current + 1 ) {
+            moves++;
+            next++;
+        }
+    }
+    return moves;
 }
 
-console.log(arrayChange([1, 1, 1]));
+// console.log(arrayChange([1, 1, 1]));
