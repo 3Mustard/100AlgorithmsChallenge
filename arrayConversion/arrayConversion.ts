e95836a3eed2e814c963ff5b7ebae60746b409ad
3Mustard <m23.cullen@gmail.com>
2020-06-16 14:04:02 -0400

    refactored arrayConversions solution

--- arrayConversion/arrayConversion.ts
+++ arrayConversion/arrayConversion.ts
@@ -12,7 +12,7 @@ function arrayConversion(inputArray: number[]): number {
 function iterationAction(inputArray: number[], action: string): number[] {
     const result: number[] = [];
 
-    for ( let i = 0; i < inputArray.length && inputArray.length >= 2; i += 2 ) {
+    for ( let i = 0; i < inputArray.length; i += 2 ) {
         switch(action) {
             case 'ODD_ITERATION':
                 result.push(inputArray[i] + inputArray[i + 1]);
